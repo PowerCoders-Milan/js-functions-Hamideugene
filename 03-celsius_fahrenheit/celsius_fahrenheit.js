103058531d2646de88a4edc76c54d3cd808eb48b
Hamideugene <hamideugene@1F-07.edu-il.it>
2021-06-16 16:33:19 +0200

    nuovo assignment  celsius_fahrenheit.js

--- 03-celsius_fahrenheit/celsius_fahrenheit.js
+++ 03-celsius_fahrenheit/celsius_fahrenheit.js
@@ -1,12 +1,26 @@
 // define a function multiplyByNineFifths(number)
-
+function multiplyByNineFifths(number) {
+    return number*(9/5)
+}
+console.log(multiplyByNineFifths(5))
     //return number*(9/5)
 
 // define a function getFahrenheit(celsius)
-
+function getFahrenheit(celsius) {
+    return multiplyByNineFifths(celsius) + 32;
+}
+ console.log(getFahrenheit(6))
     // return multiplyByNineFifths(celsius) + 32;
 
-console.log() // call getFahrenheit();
+//console.log() // call getFahrenheit();
 
+// do the same thing starting from Fahrenheit to Celsius;
+function getcelsius(fahrenheit){
+    return (fahrenheit-32)
+}
+console.log(getcelsius(6))
 
-// do the same thing starting from Fahrenheit to Celsius
+function multiplyByfiveninth(number) {
+    return number*(5/9)
+}
+console.log(multiplyByfiveninth(4))
